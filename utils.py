b0618470717bdb6dd50f83d1db26b320279d0fc2
Amado de Jesús Vázquez Acuñas <85312561+Jesus-Vazquez-A@users.noreply.github.com>
2023-09-28 16:29:36 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,19 +17,18 @@ import warnings
 
 
 def clean_text(text):
-    # Eliminar emojis
+ 
     text = re.sub(r'\\x[a-zA-Z0-9]+', '', text)
     text = re.sub(r'\\u[a-zA-Z0-9]+', '', text)
     text = re.sub(r'\\U[a-zA-Z0-9]+', '', text)
     text = re.sub(r'\\n', ' ', text)
-    
-    # Eliminar URLs
+
     text = re.sub(r'http\S+|www\S+|https\S+', '', text)
     
-    # Eliminar caracteres no alfabéticos y números
+
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     
-    # Eliminar espacios en blanco adicionales
+
     text = re.sub(r'\s+', ' ', text).strip()
     
     return text
@@ -44,27 +43,16 @@ def remove_html_tags(text):
     return cleaned_text
 
 
-# In[25]:
 
-
-def correct_spelling(text):
-   
-    spell = SpellChecker()
-  
-    return spell.correction(text)
-
-
-# In[26]:
 
 
 def remove_special_characters(text):
-    # Eliminar signos de puntuación
+
     text = text.translate(str.maketrans('', '', string.punctuation))
-    
-    # Eliminar otros caracteres especiales
+
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     
-    # Eliminar espacios en blanco adicionales
+
     text = re.sub(r'\s+', ' ', text).strip()
     
     return text
